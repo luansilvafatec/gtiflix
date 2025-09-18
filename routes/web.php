692953596917b12fa8db52285e798b5c2b08bcb1
luansilvafatec <luan.silva99@fatec.sp.gov.br>
2025-09-18 10:47:48 -0300

    instalacao tailwind e teste de rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/luan', function() {
+    echo 'Olá Luan';
+});
+
+Route::get('/filmes/{filme}', function($filme) {
+    echo 'Acessando dados do filme: '. $filme;
+});
+
+Route::view('/conheca', 'sobre');
